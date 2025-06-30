26e29b90d32f44d516f869db934a522cfedf92c1
Cillian Barron <barroncillian@gmail.com>
2025-06-30 23:02:42 +0100

    feat: add analytics & speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { ThemeProvider } from 'next-themes';
 import { SanityLive } from '@/sanity/lib/live';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -28,6 +30,8 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <SanityLive />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
